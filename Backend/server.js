4f044324ab6ac14e0fec255e7e9a31bceaf1a0da
Hari Thapa <harithapa4654@gmail.com>
2026-05-19 12:04:43 +0530

    ai service

--- Backend/server.js
+++ Backend/server.js
@@ -7,8 +7,10 @@ dotenv.config();
 
 import app from "./src/app.js"
 import conntecToDb from "./src/config/database.js";
+import { testAi } from "./src/services/ai.service.js";
 
 conntecToDb()
+testAi()
 
 app.listen(3000 , ()=> {
     console.log("server is running on port 3000");
---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,14 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash-lite",
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+export async function testAi(){
+    model.invoke("what is mern stack?").then((response) => {
+        console.log(response.text);
+    });
+}
+
+ 
