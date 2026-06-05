94ec637541774677b8fdf07f2d7290752f388a87
Hari Thapa <harithapa4654@gmail.com>
2026-06-05 22:01:06 +0530

    port

--- Backend/src/sockets/server.socket.js
+++ Backend/src/sockets/server.socket.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (httpServer) => {
     io = new Server(httpServer, {
         cors: {
-            origin: "http://localhost:5173", // Adjust this to your frontend URL
+            origin: "http://localhost:5173", // Adjust this to your frontend URL and port
             credentials: true,
         },
     });
