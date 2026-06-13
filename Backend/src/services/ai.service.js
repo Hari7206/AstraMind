c845f5e1e4dbe52e04a471da1ea9270499eba4f8
Hari Thapa <harithapa4654@gmail.com>
2026-06-13 21:37:00 +0530

    internet tool

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,6 +1,13 @@
 import "dotenv/config";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage , SystemMessage , AIMessage} from "@langchain/core/messages";
+import { HumanMessage, SystemMessage, AIMessage } from "@langchain/core/messages";
+import { tool } from "@langchain/core/tools";
+import * as z from "zod"
+import {createAgent} from "langchain"
+import { searchInternet } from "./internet.service.js";
+
+
+
 
 const model = new ChatMistralAI({
   model: "mistral-large-latest",
@@ -8,16 +15,48 @@ const model = new ChatMistralAI({
   temperature: 0,
 });
 
+
+const searchInternetTool = tool(
+  searchInternet,
+  {
+    name: "searchInternet",
+    description: "use this tool to get the latest information from the internet",
+    schema: z.object({
+      query: z.string().describe("The search query to look up to the internet")
+    })
+
+  }
+)
+
+const agent = createAgent({
+  model: model,
+  tools: [searchInternetTool]
+})
+
 export async function generateResponse(messages) {
-  const response = await model.invoke( messages.map((msg) => {
-    if (msg.role === "user") {
-      return new HumanMessage(msg.content); 
-    } else if (msg.role === "ai") {
-      return new AIMessage(msg.content); 
-    }
-  })
-  );
-  return response.text;
+  const response = await agent.invoke({
+    messages: [
+     new SystemMessage(`
+You MUST use the searchInternet tool for:
+- latest news
+- current events
+- recent updates
+- anything time sensitive
+
+Do not answer from memory.
+`),
+
+      ...messages.map((msg) => {
+        if (msg.role === "user") {
+          return new HumanMessage(msg.content);
+        } else if (msg.role === "ai") {
+          return new AIMessage(msg.content);
+        }
+      }),
+    ],
+  });
+
+  return response.messages[response.messages.length - 1].text;
 }
 
 export async function generateTitle(message) {
@@ -27,7 +66,7 @@ export async function generateTitle(message) {
       user will provide you with a message, and you will generate a title that captures the essence of the message in a few words. The title should be clear, engaging, and relevant to the content of the message.
       `),
     new HumanMessage(` Generate a title for the following message:
-      ${message}`)  
+      ${message}`)
   ]);
   return response.text;
 }
